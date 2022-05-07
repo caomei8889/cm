7985ec12f772840c88455169ac7b14a81a0c977b
caomei8889 <105102426+caomei8889@users.noreply.github.com>
2022-05-07 16:14:29 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,23 @@
+(function(w) {
+    function enterLine() {
+        document.getElementById("btnEnter1").onclick = function() {
+            window.open('http://as.asfdf54ds4a65f.cc', "_blank");
+        }
+        document.getElementById("btnEnter2").onclick = function() {
+            window.open('http://4d.4d5s46g5f4s6g5.xyz', "_blank");
+        }
+        document.getElementById("btnEnter3").onclick = function() {
+            window.open('http://r4.r4e6w545f1s32d.info', "_blank");
+        }
+        document.getElementById("btnEnter4").onclick = function() {
+            window.open('http://1f.1f5s64f56gd4h5.co', "_blank");
+        }
+        document.getElementById("btnEnter5").onclick = function() {
+            window.open('http://6g.6gf4h564j654mn.club', "_blank");
+        }
+    }
+    function init() {
+        enterLine();
+    }
+    window.addEventListener("DOMContentLoaded", init, !1);
+})(window);

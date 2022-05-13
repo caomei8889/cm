6c4fe196c0d9b782e5b465d8d831f5cf9dbbc259
caomei8889 <105102426+caomei8889@users.noreply.github.com>
2022-05-13 13:41:49 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,19 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('http://as.asfdf54ds4a65f.cc', "_blank");
+            window.open('http://we.weiruwifewgsyu.cc', "_blank");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('http://4d.4d5s46g5f4s6g5.xyz', "_blank");
+            window.open('http://fd.fdfa4564d5f4sf.cc', "_blank");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('http://r4.r4e6w545f1s32d.info', "_blank");
+            window.open('http://6h.6h545564654etr.xyz', "_blank");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('http://1f.1f5s64f56gd4h5.co', "_blank");
+            window.open('http://dv.dv312b13c2v1n5.xyz', "_blank");
         }
         document.getElementById("btnEnter5").onclick = function() {
-            window.open('http://6g.6gf4h564j654mn.club', "_blank");
+            window.open('http://f4.f46dhg54d6g5f4.info', "_blank");
         }
     }
     function init() {
